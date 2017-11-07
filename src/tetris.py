c493d31596feaa4163283bd3884c204a0a2db601
yuduki <rtccb.yuduki24@gmail.com>
2017-11-08 00:07:23 +0900

    [fix]テトロミノの素材を表示. とりあえず並べて表示した感. きちんとテトロミノになるように一定のパターンで表示する

--- src/tetris.py
+++ src/tetris.py
@@ -12,12 +12,38 @@ class Tetris:
         pygame.init()
         screen = pygame.display.set_mode(SCR_RECT.size)
         pygame.display.set_caption(u"Tetris")
+
+        # 画像の読み込み
+        self.load_images()
+
+        # ゲームオブジェクトを初期化
+        self.init_game()
+
         # メインループ開始
         clock = pygame.time.Clock()
         while True:
             clock.tick(60)
+            self.update()
+            self.draw(screen)
             pygame.display.update()
             self.key_handler()
+
+    def init_game(self):
+        """ゲームオブジェクトの初期化"""
+        self.all = pygame.sprite.RenderUpdates()
+        Tetromino.containers = self.all
+
+        for x in range(0, 7):
+            Tetromino(x, self.tetromino_img[x])
+
+    def update(self):
+        """ゲーム状態の更新"""
+        self.all.update()
+
+    def draw(self, screen):
+        """描画"""
+        screen.fill((10, 10, 10))
+        self.all.draw(screen)
 
     def key_handler(self):
         """キーハンドラー"""
@@ -25,7 +51,19 @@ class Tetris:
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+    
+    def load_images(self):
+        """イメージのロード"""
+        self.tetromino_img = split_image(load_image("tetromino.png"), 7)
 
+class Tetromino(pygame.sprite.Sprite):
+    """テトロミノ"""
+    def __init__(self, x, image):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.left = SCR_RECT.left + x * self.rect.width
+        self.reload_timer = 0
 
 if __name__ == "__main__":
     Tetris()
