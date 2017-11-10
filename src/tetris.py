a3437408ca6e7f57e1e807ae5281b565b5bcd2bb
yuduki <rtccb.yuduki24@gmail.com>
2017-11-11 00:13:18 +0900

    [fix]フィールドを少しいい感じに修正.

--- src/tetris.py
+++ src/tetris.py
@@ -41,11 +41,12 @@ class Tetris:
         Field.image = self.wall_img
         Field.screen = screen
 
-        Field()
+        self.field1 = Field(4, 4)
         self.acctive_tetriminos = []
+        self.debug_count = 1
         # デバッグ用.7種類置く
-        for color in range(7):
-            self.acctive_tetriminos.append(Tetrimino(40+(5 * color * CELL_SIZE), 60, color))
+        for color in range(self.debug_count):
+            self.acctive_tetriminos.append(Tetrimino(color))
 
     def update(self):
         """ゲーム状態の更新"""
@@ -69,13 +70,13 @@ class Tetris:
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN and event.key == K_SPACE:
-                for n in range(7):
+                for n in range(self.debug_count):
                     self.acctive_tetriminos[n].spin(RIGHT)
             elif event.type == KEYDOWN and event.key == K_RIGHT:
-                for n in range(7):
+                for n in range(self.debug_count):
                     self.acctive_tetriminos[n].move(RIGHT)
             elif event.type == KEYDOWN and event.key == K_LEFT:
-                for n in range(7):
+                for n in range(self.debug_count):
                     self.acctive_tetriminos[n].move(LEFT)
     def load_images(self):
         """イメージのロード"""
@@ -88,7 +89,7 @@ class Tetrimino():
                       (TETRIMINO_J1, TETRIMINO_J2, TETRIMINO_J3, TETRIMINO_J4), \
                       (TETRIMINO_Z1, TETRIMINO_Z2), (TETRIMINO_S1, TETRIMINO_S2), \
                       (TETRIMINO_I1, TETRIMINO_I2), (TETRIMINO_O1, ) )
-    def __init__(self, x, y, color):
+    def __init__(self, color):
         self.PATTERN = self.TETRIMINO_KIND[color]
         self.PATTERN_COUNT = len(self.PATTERN)
         self.pattern_number = 0
@@ -96,13 +97,12 @@ class Tetrimino():
         print(self.current_pattern)
         self.block_row = len(self.current_pattern)
         self.block_col = len(self.current_pattern[0])
-        self.x = x
-        self.y = y
+        self.x, self.y = 3, 0
         self.blocks = []
         for i in range(self.block_row):
             for j in range(self.block_col):
                 if self.current_pattern[i][j] == 1:
-                    self.blocks.append(Block((x+CELL_SIZE*j), (y+CELL_SIZE*i), self.tetrimino_img[color]))
+                    self.blocks.append(Block((self.x+j), (self.y+i), self.tetrimino_img[color]))
     def spin(self, direction):
         # ただただ回転させるコード.
         # next_pattern = [[0 for i in range(self.block_row)] for j in range(self.block_col)]
@@ -127,9 +127,9 @@ class Tetrimino():
 
     def move(self, direction):
         if direction == RIGHT:
-            self.x += CELL_SIZE
+            self.x += 1
         elif direction == LEFT:
-            self.x -= CELL_SIZE
+            self.x -= 1
         self.__update()
 
     def __update(self):
@@ -138,7 +138,7 @@ class Tetrimino():
         for i in range(self.block_row):
             for j in range(self.block_col):
                 if self.current_pattern[i][j] == 1:
-                    self.blocks[num].set_position((self.x+CELL_SIZE*j), (self.y+CELL_SIZE*i))
+                    self.blocks[num].set_position((self.x+j), (self.y+i))
                     num += 1
 
 class Block(pygame.sprite.Sprite):
@@ -146,19 +146,20 @@ class Block(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.left = x
-        self.rect.top = y
+        self.rect.left = x * CELL_SIZE
+        self.rect.top = y * CELL_SIZE
     def set_position(self, x, y):
-        self.rect.left = x
-        self.rect.top = y
+        self.rect.left = x * CELL_SIZE
+        self.rect.top = y * CELL_SIZE
 
 class Field():
-    def __init__(self):
+    def __init__(self, top, left):
         # 描画用と実際にブロックを置く用の2つ用意.
         # 周りの壁用に+2する.
         self.image_field = [[0 for i in range(FIELD_COL+2)] for j in range(FIELD_ROW+2)]
         self.block_field = self.image_field[:][:]
-
+        self.TOP = top
+        self.LEFT = left
         # 両端を埋める.
         for row in range(FIELD_ROW+2):
             self.image_field[row][0] = -1
@@ -175,14 +176,14 @@ class Field():
         for y in range(FIELD_ROW+2):
             for x in range(FIELD_COL+2):
                 if self.image_field[y][x] == -1:
-                     self.block_field[y][x] = Block(x*CELL_SIZE+FIELD_TOP,  y*CELL_SIZE+FIELD_LEFT, self.image)
+                     self.block_field[y][x] = Block(x+self.TOP,  y+self.LEFT, self.image)
 
     def update(self):
         for y in range(FIELD_ROW):
             for x in range(FIELD_COL):
                 if [image_field][y][x] == -1:
                      #self.screen.blit(self.image, (x*CELL_SIZE+FIELD_TOP,  y*CELL_SIZE+FIELD_LEFT))
-                     self.screen.blit(self.image, (x*CELL_SIZE,  y*CELL_SIZE))
+                     self.screen.blit(self.image, ((x+self,TOP)*CELL_SIZE,  (y+self.LEFT)*CELL_SIZE))
 
 if __name__ == "__main__":
     Tetris()
